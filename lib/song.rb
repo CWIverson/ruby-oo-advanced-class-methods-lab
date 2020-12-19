4f0e6bad4795900b43a680e1cd84bd00fc6e2f87
christian iverson <chris.w.iverson@gmail.com>
2020-12-18 19:09:33 -0800

    commit tree a236762d73c208e6cd851645040c4a155a5f1fbb

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,48 @@ class Song
   def save
     self.class.all << self
   end
+  def self.create
+    song= self.new
+    @@all << song
+    song
+  end
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    new_song
+  end
+  def self.create_by_name(name)
+    new_song = self.new
+    new_song.name= name
+    @@all << new_song
+    new_song
+  end
+  def self.find_by_name(name)
+    @@all.find{|song|song.name == name}
+  end
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+  def self.new_from_filename(filename)
+    array = filename.split(/\s\-\s|\./)
+    array.pop 
+    new_song = self.new
+    new_song.name = array[1]
+    new_song.artist_name = array[0]
+    new_song
 
+  end
+  def self.create_from_filename(filename)
+    self.all<<self.new_from_filename(filename)
+  end
+  def self.destroy_all
+    self.all.clear
+  end
 end
